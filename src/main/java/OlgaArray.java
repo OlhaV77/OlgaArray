3a42b0062edbb056b145bc336b97911874ce5801
olgaV77 <olga.v1977@gmail.com>
2021-04-03 08:54:55 -0400

    finish last test find by letter

--- src/main/java/OlgaArray.java
+++ src/main/java/OlgaArray.java
@@ -20,10 +20,10 @@ public class OlgaArray {
             }
         }
 
-        if (  countOfEmptySpace  < 1) {              // no empty space
-            LetterSummary[] letterSummaries = new LetterSummary[ collection.length + 1];
+        if (countOfEmptySpace < 1) {              // no empty space
+            LetterSummary[] letterSummaries = new LetterSummary[collection.length + 1];
 
-            for (int j = 0; j < collection.length ; j++) {    // go along the length collections array
+            for (int j = 0; j < collection.length; j++) {    // go along the length collections array
                 letterSummaries[j] = collection[j];           // copy array, by collection array with index   and send in letterSummary array with same index
             }
 
@@ -34,7 +34,7 @@ public class OlgaArray {
     }
 
     public LetterSummary[] getAll() {
-        return collection ;
+        return collection;
     }
 
     public LetterSummary get(int index) {
@@ -47,10 +47,20 @@ public class OlgaArray {
     }
 
     public LetterSummary findByLetter(String letter) {
-        collection = new LetterSummary[letter.length()];
+        int index = 0;
+        for (int i = 0; i < collection.length; i++) {               // check object array
+            if (letter.equals(collection[i].letter)) {              // if a letter equals  a object
+                return collection[i];                               // return object
+            }
+            index = i;                                            //  compare index with i (i = what is the count object)
+        }
+        collection = new LetterSummary[letter.length()];          // new object
 
-        return null;
+        if (letter.equals(collection[index].letter)) {             // if a letter equals new object
+            return collection[index];                              //return object
+        }
+
+        return collection[index];
+
     }
-
-
 }
