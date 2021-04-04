e4783cff2e511d4877e66b27c1bdf700d415716b
olgaV77 <olga.v1977@gmail.com>
2021-04-04 10:35:49 -0400

    new test

--- src/test/java/OlgaArrayTest.java
+++ src/test/java/OlgaArrayTest.java
@@ -67,8 +67,6 @@ public class OlgaArrayTest {
         assertThat(actual.count).isEqualTo(2);
     }
 
-
-
     @Test
     public void findByLetter_whenRecordExists_returnsTheRecord() {
         OlgaArray subject = new OlgaArray();
@@ -80,8 +78,6 @@ public class OlgaArrayTest {
         assertThat(actual.letter).isEqualTo("a");
         assertThat(actual.count).isEqualTo(1);
     }
-
-
 
     @Test
     public void findByLetter_when3RecordExists_returnsTheRecord() {
@@ -97,16 +93,122 @@ public class OlgaArrayTest {
         assertThat(actual.count).isEqualTo(1);
     }
 
+    @Test
+    public void findByLetter_whenRecordDoesNotExists_returnsNull() {
+        OlgaArray subject = new OlgaArray();
+
+        subject.add(buildLetterSummary("b", 10));
+        subject.add(buildLetterSummary("a", 1));
+        subject.add(buildLetterSummary("z", 2));
+
+        LetterSummary actual = subject.findByLetter("q");
+
+        assertThat(actual).isNull();
+    }
+
+    @Test
+    public void findByLetter_when3RecordExistsAndMultipleCalls_returnsTheRecord() {
+        OlgaArray subject = new OlgaArray();
+
+        subject.add(buildLetterSummary("b", 10));
+        subject.add(buildLetterSummary("a", 1));
+        subject.add(buildLetterSummary("z", 2));
+
+        subject.findByLetter("q");
+
+        assertThat(subject.findByLetter("a").letter).isEqualTo("a");
+        assertThat(subject.findByLetter("b").letter).isEqualTo("b");
+        assertThat(subject.findByLetter("z").letter).isEqualTo("z");
+    }
+/*
+    @Test
+    public void remove_whenOneRecordExists_containsZeroRecords() {
+        OlgaArray subject = new OlgaArray();
+
+        subject.add(buildLetterSummary("b", 10));
+
+        subject.remove(0);
+
+        LetterSummary[] actual = subject.getAll();
+
+        assertThat(actual).hasSize(0);
+    }
+
+    @Test
+    public void remove_whenTwoRecordExists_contains1Records() {
+        OlgaArray subject = new OlgaArray();
+
+        subject.add(buildLetterSummary("b", 10));
+        subject.add(buildLetterSummary("d", 10));
+
+        subject.remove(0);
+
+        LetterSummary[] actual = subject.getAll();
+
+        assertThat(actual).hasSize(1);
+        assertThat(actual[0].letter).isEqualTo("d");
+    }
+
+    @Test
+    public void remove_when3RecordExistsAndRemoveFirstRecord_containsOnlyRemainingRecords() {
+        OlgaArray subject = new OlgaArray();
+
+        subject.add(buildLetterSummary("b", 10));
+        subject.add(buildLetterSummary("a", 1));
+        subject.add(buildLetterSummary("z", 2));
+
+        subject.remove(0);
+
+        LetterSummary[] actual = subject.getAll();
+
+        assertThat(actual).hasSize(2);
+
+        assertThat(actual[0].letter).isEqualTo("a");
+        assertThat(actual[1].letter).isEqualTo("z");
+    }
+
+    @Test
+    public void remove_when3RecordExistsAndRemoveSecondRecord_containsOnlyRemainingRecords() {
+        OlgaArray subject = new OlgaArray();
+
+        subject.add(buildLetterSummary("b", 10));
+        subject.add(buildLetterSummary("a", 1));
+        subject.add(buildLetterSummary("z", 2));
+
+        subject.remove(1);
+
+        LetterSummary[] actual = subject.getAll();
+
+        assertThat(actual).hasSize(2);
+
+        assertThat(actual[0].letter).isEqualTo("b");
+        assertThat(actual[1].letter).isEqualTo("z");
+    }
+
+    @Test
+    public void remove_when3RecordExistsAndRemoveThirdRecord_containsOnlyRemainingRecords() {
+        OlgaArray subject = new OlgaArray();
+
+        subject.add(buildLetterSummary("b", 10));
+        subject.add(buildLetterSummary("a", 1));
+        subject.add(buildLetterSummary("z", 2));
+
+        subject.remove(2);
+
+        LetterSummary[] actual = subject.getAll();
+
+        assertThat(actual).hasSize(2);
+
+        assertThat(actual[0].letter).isEqualTo("b");
+        assertThat(actual[1].letter).isEqualTo("a");
+    }
 
 
+ */
     private LetterSummary buildLetterSummary(String letter, int count) {
         LetterSummary letterSummary = new LetterSummary();
         letterSummary.letter = letter;
         letterSummary.count = count;
         return letterSummary;
     }
-
-
-
-
 }
